e0f7db234ace87bf1e46dec449d5c6f0cbecf97a
perarasanvijay12 <perarasanvijay5000@gmail.com>
2023-05-04 16:30:59 +0530

    first commit

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -3,7 +3,6 @@ def call(Map stageParams) {
     checkout([
         $class: 'GitSCM',
         branches: [[name:  stageParams.branch ]],
-        userRemoteConfigs: [[ credentialsId: stageParams.credentialsId ]]
         userRemoteConfigs: [[ url: stageParams.url ]]
     ])
   }
